bd65c86a0b5eb8c4c9332e06ed2e304b1d4d25cb
Melinda Hardin <melindawad@gmail.com>
2018-02-09 20:44:08 -0600

    assignValue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,10 @@
 
 
 var counter = 0;
-var randomNumber = Math.floor(Math.random()*120)+19;
-var crystalValues =Math.floor(Math.random()*12) +1;
+var randomNumber=Math.floor(Math.random()*120)+19;
 
-
-$("#random-number").text(randomNumber);
-
-
-    $("#sapphire").on("click", function(){
+function assignValue (){
+    var crystalValues=Math.floor(Math.random()*12) +1;
         counter+=crystalValues;
 
         alert("Your score is: " + counter + " points.");
@@ -21,11 +17,33 @@ $("#random-number").text(randomNumber);
         else if (counter > randomNumber){
             alert ("Too high!")
         }
+}
 
-        
+
+
+$("#random-number").text(randomNumber);
+$("#total-score").text(counter);
+
+
+    $("#sapphire").on("click", function(){
+
+        assignValue () 
+    });
+
+    $("#RedGem").on("click", function(){
+        assignValue ()
 
     });
 
+    $("#Peridot").on("click", function(a){
+        assignValue ()
+
+    });
+
+    $("#Amethyst").on("click", function(){
+        assignValue ()
+
+    });
 
 
 
