cb77793693700ea60eee4044215de916c7330b54
Melinda Hardin <melindawad@gmail.com>
2018-02-09 19:14:04 -0600

    Feb 9

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,32 @@
-$("#sapphire").on("click", function(){
-    alert("test");
-})
+
+
+
+var counter = 0;
+var randomNumber = Math.floor(Math.random()*120)+19;
+var crystalValues =Math.floor(Math.random()*12) +1;
+
+
+$("#random-number").text(randomNumber);
+
+
+    $("#sapphire").on("click", function(){
+        counter+=crystalValues;
+
+        alert("Your score is: " + counter + " points.");
+
+        if(counter === randomNumber){
+            alert("YOU WIN!")
+        }
+
+        else if (counter > randomNumber){
+            alert ("Too high!")
+        }
+
+        
+
+    });
+
+
+
+
+
